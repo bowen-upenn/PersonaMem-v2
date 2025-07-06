30186bfcc462305dd63b5957199e26fe4076d5b4
bowen-upenn <bwjiang@seas.upenn.edu>
2025-07-06 03:28:31 -0400

    rename script

---
+++ scripts/run_convo_gen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python main.py \
+  --model gpt-4.1 \
+  --step generate_data \
+  --conv_output_path data/interactions.json \
+  --num_persona 1 \
+  --data_types knowledge_query \
+  --clean \
+  --verbose
+
+  # --data_types personal_email professional_email creative_writing professional_writing chat_message translation trouble_consult knowledge_query \
